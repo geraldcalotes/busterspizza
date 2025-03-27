ede16d6e9dca5d58519cf21914b1353dd031e4f9
Gerald Calotes <gerald.calotes.dev@gmail.com>
2025-03-26 19:32:06 -0600

    Added branch dropdown to loginUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,20 @@ function App() {
   return (
     <>
       <div>
-         <div class="login-box">
-        <div class="login-header">
+         <div className="login-box">
+        <div className="login-header">
             <header><img src={logo} alt="Buster's Pizza logo" /></header>
         </div>
+        <div className="input-box">
+            <select class="input-field" name="branch" required style={{ height: '50px', padding: '0 15px',marginBottom:'50px' }}>
+                <option value="" disabled selected>Select Branch</option>
+                <option value="downtown">Downtown</option>
+                <option value="uptown">Uptown</option>
+                <option value="eastside">Eastside</option>
+                <option value="westside">Westside</option>
+            </select>
+        </div>
+        
         <div class="input-box">
             <input type="text" class="input-field" placeholder="EmployeeID" autocomplete="off" required />
         </div>
